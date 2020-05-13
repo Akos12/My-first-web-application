d5fdd60c8bb79a2f5f47ec07e90277e3e41cf423
Akos12 <64487206+Akos12@users.noreply.github.com>
2020-05-13 17:42:28 +0200

    k

---
+++ scripts.js
@@ -0,0 +1,6 @@
+let imagesdata = {
+    photo: 'Images/hegy.jpg',
+    title: 'big mountain',
+    description: 'ez egy gyönyörű hegy'
+  };
+  $('#hegy').attr('file:///C:/Users/%C3%81kos/OneDrive/Desktop/code/img/Angel-Falls-Venezuela_GettyImages-165513023.webp', data.photo);
